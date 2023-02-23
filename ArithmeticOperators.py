f9505d11495f2bd91dbdc985cc3f51985695614d
Shanmukhkasilanka <105287809+Shanmukhkasilanka@users.noreply.github.com>
2023-02-23 20:14:42 +0530

    Shanmukh
    This program is implementing all the arithmetic operators which exists in python programming language

---
+++ ArithmeticOperators.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+print('The addition of a and b is ',a+b)
+print('The subtraction of a and b is ',a-b)
+print('The multiplication of a and b is ',a*b)
+print('The divison of a and b is ',a/b)
+print('The remainder of a and b is ',a%b)
+print('The exponential (power) of a and b is ',a**b)
+print('The floor division of a and b is ',a//b)
